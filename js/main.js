a471e74076e37beb873fb72bb69b49d00f05a5df
Dyllan Murray <dmurray@innovateteam.com>
2019-05-03 12:53:56 -0600

    could this be?

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ function createMap(){
     let wine = L.layerGroup()
     let university = L.markerClusterGroup()
     let stateParks = L.layerGroup()
-    // let airports = L.layerGroup()
+    var airports = L.layerGroup()
+
 
 
     //dark matter basemap
@@ -51,7 +52,7 @@ function createMap(){
 			onEachFeature: addMyData,
 			color: 'grey',
 			fillColor: 'green' ,
-            fillOpacity: .6,
+            fillOpacity: .4,
             outline: 'purple',
 			weight: .1,
 			opacity: 1
@@ -223,6 +224,29 @@ function createMap(){
                 }
             }).addTo(wine)
         });
+
+
+
+        /////airports
+        var airIcon = L.icon({
+            iconUrl: 'img/airport.png',
+            // shadowUrl: 'img/fishing.png',
+        
+            iconSize:     [15, 15], // size of the icon
+            // shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+            // shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+        });
+        
+        wineLayer = jQuery.getJSON( "data/airports.geojson", function(json){
+            L.geoJSON(json, {
+                pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {icon: airIcon}).bindPopup("<p><b>Airport: </b>" + feature.properties.Airport+ "</p><p><b>Ridership in 2015: </b>" + feature.properties.year_2015);
+                }
+            }).addTo(airports)
+        });
+        //////
 ///////// TOPOJSON STATE PARKS
 L.TopoJSON = L.GeoJSON.extend({
     addData: function (data) {
@@ -351,287 +375,287 @@ L.TopoJSON = L.GeoJSON.extend({
     
     L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
 
-   getData(airports);
+//    getData(airports);
     // getData(map);
     
 };
 
-var airports = L.layerGroup();
+
 
 //Add circle markers for point features to the map
-function createPropSymbols(data, map, attributes){
-    //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(data, {
-        pointToLayer: function(feature, latlng){
-            return pointToLayer(feature, latlng, attributes);
-        }
-    }).addTo(map);
-};
+// function createPropSymbols(data, map, attributes){
+//     //create a Leaflet GeoJSON layer and add it to the map
+//     L.geoJson(data, {
+//         pointToLayer: function(feature, latlng){
+//             return pointToLayer(feature, latlng, attributes);
+//         }
+//     }).addTo(map);
+// };
 
-//Step 2: Import GeoJSON data
-function getData(map){
-    //load the data
-    $.ajax("data/airports.geojson", {
-        dataType: "json",
-        success: function(response){
-            //call function to create proportional symbols
-            var attributes = processData(response)
-            createPropSymbols(response, map, attributes);
-            createSequenceControls(map, attributes);
-            createLegend(map, attributes);
-        }
-    });
-};
+// //Step 2: Import GeoJSON data
+// function getData(map){
+//     //load the data
+//     $.ajax("data/airports.geojson", {
+//         dataType: "json",
+//         success: function(response){
+//             //call function to create proportional symbols
+//             var attributes = processData(response)
+//             createPropSymbols(response, map, attributes);
+//             createSequenceControls(map, attributes);
+//             createLegend(map, attributes);
+//         }
+//     });
+// };
 
-function calcPropRad(attValue) {
-    //scale factor to adjust symbol size evenly
-    var scaleFactor = .0029;
-    //area based on attribute value and scale factor
-    var area = attValue * scaleFactor;
-    //radius calculated based on area
-    var radius = Math.sqrt(area/Math.PI);
+// function calcPropRad(attValue) {
+//     //scale factor to adjust symbol size evenly
+//     var scaleFactor = .0029;
+//     //area based on attribute value and scale factor
+//     var area = attValue * scaleFactor;
+//     //radius calculated based on area
+//     var radius = Math.sqrt(area/Math.PI);
 
-    return radius;
+//     return radius;
 
-};
+// };
 
-function pointToLayer(feature, latlng, attributes){
-    //Determine which attribute to visualize with proportional symbols
-    var attribute = attributes[0];
-    console.log(attribute)
+// function pointToLayer(feature, latlng, attributes){
+//     //Determine which attribute to visualize with proportional symbols
+//     var attribute = attributes[0];
+//     console.log(attribute)
 
-    //create marker options
-    var options = {
-        fillColor: "#0066cc",
-        color: "#ffed10",
-        weight: 1.5,
-        opacity: 1,
-        fillOpacity: 0.5
-    };
+//     //create marker options
+//     var options = {
+//         fillColor: "#0066cc",
+//         color: "#ffed10",
+//         weight: 1.5,
+//         opacity: 1,
+//         fillOpacity: 0.5
+//     };
 
-    //For each feature, determine its value for the selected attribute
-    var attValue = Number(feature.properties[attribute]);
+//     //For each feature, determine its value for the selected attribute
+//     var attValue = Number(feature.properties[attribute]);
 
-    //Give each feature's circle marker a radius based on its attribute value
-    options.radius = calcPropRad(attValue);
+//     //Give each feature's circle marker a radius based on its attribute value
+//     options.radius = calcPropRad(attValue);
 
-    var layer = L.circleMarker(latlng, options);
+//     var layer = L.circleMarker(latlng, options);
 
-    //build popup content string
-    var popupContent = "<p><b>Airport:</b> " + feature.properties.Airport + "</p>";
+//     //build popup content string
+//     var popupContent = "<p><b>Airport:</b> " + feature.properties.Airport + "</p>";
 
-    //add formatted attribute to popup content string
-    // var year = attribute.split("_")[1];
-    // popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + "</p>";
+//     //add formatted attribute to popup content string
+//     // var year = attribute.split("_")[1];
+//     // popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + "</p>";
     
-    //bind the popup to the circle marker
-    layer.bindPopup(popupContent);
+//     //bind the popup to the circle marker
+//     layer.bindPopup(popupContent);
 
-    //return the circle marker to the L.geoJson pointToLayer option
-    return layer;
-};
+//     //return the circle marker to the L.geoJson pointToLayer option
+//     return layer;
+// };
 
-function createSequenceControls(map, attributes){
-    var SequenceControl = L.Control.extend({
-        options: {
-            position: 'bottomleft'
-        },
+// function createSequenceControls(map, attributes){
+//     var SequenceControl = L.Control.extend({
+//         options: {
+//             position: 'bottomleft'
+//         },
 
-       onAdd: function (map) {
-            // create the control container with a particular class name
-            var container = L.DomUtil.create('div', 'sequence-control-container');
+//        onAdd: function (map) {
+//             // create the control container with a particular class name
+//             var container = L.DomUtil.create('div', 'sequence-control-container');
 
-            //create range input element (slider)
-            $(container).append('<input class="range-slider" type="range">');
+//             //create range input element (slider)
+//             $(container).append('<input class="range-slider" type="range">');
 
-            //add skip buttons
-            $(container).append('<button class="skip" id="reverse" title="Previous Year">Previous</button>');
-            $(container).append('<button class="skip" id="forward" title="Next Year">Next</button>');
+//             //add skip buttons
+//             $(container).append('<button class="skip" id="reverse" title="Previous Year">Previous</button>');
+//             $(container).append('<button class="skip" id="forward" title="Next Year">Next</button>');
 
-            $(container).on('mousedown dblclick', function(e){
-				L.DomEvent.stopPropagation(e);
-            });
+//             $(container).on('mousedown dblclick', function(e){
+// 				L.DomEvent.stopPropagation(e);
+//             });
 
-            return container;
-        }
-    });
+//             return container;
+//         }
+//     });
 
-    map.addControl(new SequenceControl());
+//     map.addControl(new SequenceControl());
 
-    //set slider attributes
-    $('.range-slider').attr({
-        max: 7,
-        min: 0,
-        value: 0,
-        step: 1
-    });
+//     //set slider attributes
+//     $('.range-slider').attr({
+//         max: 7,
+//         min: 0,
+//         value: 0,
+//         step: 1
+//     });
 
-    $('.skip').click(function(){
-        //get the old index value
-        var index = $('.range-slider').val();
+//     $('.skip').click(function(){
+//         //get the old index value
+//         var index = $('.range-slider').val();
 
-        //Step 6: increment or decrement depending on button clicked
-        if ($(this).attr('id') == 'forward'){
-            index++;
-            //Step 9: pass new attribute to update symbols
-            updatePropSymbols(map, attributes[index]);
+//         //Step 6: increment or decrement depending on button clicked
+//         if ($(this).attr('id') == 'forward'){
+//             index++;
+//             //Step 9: pass new attribute to update symbols
+//             updatePropSymbols(map, attributes[index]);
 
-            //Step 7: if past the last attribute, wrap around to first attribute
-            index = index > 7 ? 0 : index;
-        } else if ($(this).attr('id') == 'reverse'){
-            index--;
-            //Step 7: if past the first attribute, wrap around to last attribute
-            index = index < 0 ? 7 : index;
-        };
+//             //Step 7: if past the last attribute, wrap around to first attribute
+//             index = index > 7 ? 0 : index;
+//         } else if ($(this).attr('id') == 'reverse'){
+//             index--;
+//             //Step 7: if past the first attribute, wrap around to last attribute
+//             index = index < 0 ? 7 : index;
+//         };
 
-        //Step 8: update slider
-        $('.range-slider').val(index);
-    });
+//         //Step 8: update slider
+//         $('.range-slider').val(index);
+//     });
     
-};
+// };
 
-function updatePropSymbols(map,attribute){
-    map.eachLayer(function(layer){
-        if (layer.feature && layer.feature.properties[attribute]){
-            //access feature properties
-            var props = layer.feature.properties;
+// function updatePropSymbols(map,attribute){
+//     map.eachLayer(function(layer){
+//         if (layer.feature && layer.feature.properties[attribute]){
+//             //access feature properties
+//             var props = layer.feature.properties;
 
-            //update each feature's radius based on new attribute values
-            var radius = calcPropRad(props[attribute]);
-            layer.setRadius(radius);
-            updateLegend(map, attribute);	
-            //add city to popup content string
-            var popupContent = "<p><b>City:</b> " + props.Airport + "</p>";
+//             //update each feature's radius based on new attribute values
+//             var radius = calcPropRad(props[attribute]);
+//             layer.setRadius(radius);
+//             updateLegend(map, attribute);	
+//             //add city to popup content string
+//             var popupContent = "<p><b>City:</b> " + props.Airport + "</p>";
 
-            //add formatted attribute to panel content string
-            var year = attribute.split("_")[1];
-            popupContent += "<p><b>Ridership in " + year + ":</b> " + props[attribute] + "</p>";
+//             //add formatted attribute to panel content string
+//             var year = attribute.split("_")[1];
+//             popupContent += "<p><b>Ridership in " + year + ":</b> " + props[attribute] + "</p>";
 
-            //replace the layer popup
-            layer.bindPopup(popupContent, {
-                offset: new L.Point(0,-radius)
-            });
-        };
-	});
+//             //replace the layer popup
+//             layer.bindPopup(popupContent, {
+//                 offset: new L.Point(0,-radius)
+//             });
+//         };
+// 	});
 	
-};
-function processData(data){
-    //empty array to hold attributes
-    var attributes = [];
+// };
+// function processData(data){
+//     //empty array to hold attributes
+//     var attributes = [];
 
-    //properties of the first feature in the dataset
-    var properties = data.features[0].properties;
+//     //properties of the first feature in the dataset
+//     var properties = data.features[0].properties;
 
-    //push each attribute name into attributes array
-    for (var attribute in properties){
-        //only take attributes with population values
-        if (attribute.indexOf("year") > -1){
-            attributes.push(attribute);
-        };
-    };
+//     //push each attribute name into attributes array
+//     for (var attribute in properties){
+//         //only take attributes with population values
+//         if (attribute.indexOf("year") > -1){
+//             attributes.push(attribute);
+//         };
+//     };
 
-    //check result
-    console.log(attributes);
+//     //check result
+//     console.log(attributes);
 
-    return attributes;
-};
+//     return attributes;
+// };
 
-function createLegend(map, attributes){
+// function createLegend(map, attributes){
     
-    var LegendControl = L.Control.extend({
-		options: {
-            position: 'bottomright'
-        },
+//     var LegendControl = L.Control.extend({
+// 		options: {
+//             position: 'bottomright'
+//         },
 
-        onAdd: function (map) {
-            // create the control container with a particular class name
-            var container = L.DomUtil.create('div', 'legend-control-container');
+//         onAdd: function (map) {
+//             // create the control container with a particular class name
+//             var container = L.DomUtil.create('div', 'legend-control-container');
 
-            //add temporal legend div to container
-            $(container).append('<div id="temporal-legend">')
+//             //add temporal legend div to container
+//             $(container).append('<div id="temporal-legend">')
 			
-			var svg = '<svg id="attribute-legend" width="180px" height="80px">';
-			//array of circle names to base loop on
-			var circles = {
-            max: 20,
-            mean: 40,
-            min: 60
-        };
+// 			var svg = '<svg id="attribute-legend" width="180px" height="80px">';
+// 			//array of circle names to base loop on
+// 			var circles = {
+//             max: 20,
+//             mean: 40,
+//             min: 60
+//         };
 
-        //loop to add each circle and text to svg string
-        for (var circle in circles){
-            //circle string
-            svg += '<circle class="legend-circle" id="' + circle + '" fill="#0066cc" fill-opacity="0.8" stroke="#ffed10" cx="30"/>';
+//         //loop to add each circle and text to svg string
+//         for (var circle in circles){
+//             //circle string
+//             svg += '<circle class="legend-circle" id="' + circle + '" fill="#0066cc" fill-opacity="0.8" stroke="#ffed10" cx="30"/>';
 
-            //text string
-            svg += '<text id="' + circle + '-text" x="65" y="' + circles[circle] + '"></text>';
-        };
-			//add attribute legend svg to container			
-			$(container).append(svg);			
-            return container;
-		}
-	});
+//             //text string
+//             svg += '<text id="' + circle + '-text" x="65" y="' + circles[circle] + '"></text>';
+//         };
+// 			//add attribute legend svg to container			
+// 			$(container).append(svg);			
+//             return container;
+// 		}
+// 	});
 
-    map.addControl(new LegendControl());
-	updateLegend(map, attributes[0]);
-};
+//     map.addControl(new LegendControl());
+// 	updateLegend(map, attributes[0]);
+// };
 
-function getCircleValues(map, attribute){
-	//start with min at highest possible and max at lowest possible number
-	var min = Infinity,
-        max = -Infinity;
+// function getCircleValues(map, attribute){
+// 	//start with min at highest possible and max at lowest possible number
+// 	var min = Infinity,
+//         max = -Infinity;
 
-    map.eachLayer(function(layer){
-        //get the attribute value
-        if (layer.feature){
-            var attributeValue = Number(layer.feature.properties[attribute]);
+//     map.eachLayer(function(layer){
+//         //get the attribute value
+//         if (layer.feature){
+//             var attributeValue = Number(layer.feature.properties[attribute]);
 			
-			//test for min
-			if (attributeValue < min){
-                min = attributeValue;
-            };
+// 			//test for min
+// 			if (attributeValue < min){
+//                 min = attributeValue;
+//             };
 
-            //test for max
-            if (attributeValue > max){
-                max = attributeValue;
-            };
-        };
-    });
+//             //test for max
+//             if (attributeValue > max){
+//                 max = attributeValue;
+//             };
+//         };
+//     });
 
-    //set mean
-    var mean = (max + min) / 2;
+//     //set mean
+//     var mean = (max + min) / 2;
 
-    //return values as an object
-    return {
-        max: max,
-        mean: mean,
-        min: min
-    };
-};
+//     //return values as an object
+//     return {
+//         max: max,
+//         mean: mean,
+//         min: min
+//     };
+// };
 
-function updateLegend(map, attribute){
-    //create dynamic title
-    var year = attribute.split("_")[1];
-    var content = "Ridership in " + year;
+// function updateLegend(map, attribute){
+//     //create dynamic title
+//     var year = attribute.split("_")[1];
+//     var content = "Ridership in " + year;
 
-    //replace legend content
-    $('#temporal-legend').html(content);
+//     //replace legend content
+//     $('#temporal-legend').html(content);
 
-    //get the max, mean, and min values as an object
-    var circleValues = getCircleValues(map, attribute);
-	for (var key in circleValues){
-        //get radius
-        var radius = calcPropRad(circleValues[key]);
+//     //get the max, mean, and min values as an object
+//     var circleValues = getCircleValues(map, attribute);
+// 	for (var key in circleValues){
+//         //get radius
+//         var radius = calcPropRad(circleValues[key]);
 
-        $('#'+key).attr({
-            cy: 55 - radius,
-            r: radius
-        });
+//         $('#'+key).attr({
+//             cy: 55 - radius,
+//             r: radius
+//         });
 
-        //Step 4: add legend text
-        $('#'+key+'-text').text(Math.round((circleValues[key]*100)/100));
-    };
-};
+//         //Step 4: add legend text
+//         $('#'+key+'-text').text(Math.round((circleValues[key]*100)/100));
+//     };
+// };
 
 $(document).ready(createMap);
 
